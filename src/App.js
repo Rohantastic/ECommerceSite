f072c088e4f79d392382a44331f5bb1066f2cdb5
rohantastic <rohantastic@outlook.com>
2023-10-14 20:52:04 +0530

    added about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/Pages/Main';
 import Cart from './components/Pages/Cart';
+import About from './components/Pages/About';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           
           <Route path='/' element={<Main/>}/>
           <Route path='/store' element={<Main/>}/>
-          <Route path='/about' element={<Main/>}/>
+          <Route path='/about' element={<About/>}/>
           <Route path='/cart' element={<Cart/>}/>
 
         </Routes>
