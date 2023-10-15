0f535c0bc92f04dcbeea0b21fdcac3bd12476111
rohantastic <rohantastic@outlook.com>
2023-10-15 10:12:23 +0530

    no-cart fix

--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -1,45 +1,52 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import "./Cart.css";
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 import { CartItemContext } from '../contexts/CartItemContext';
 
-
-
-
 const Cart = () => {
+    const cartData = useContext(CartItemContext);
 
-    const cartData = useContext(CartItemContext); 
-    console.log("carting:: ", cartData);
+    let noDataPresent = 0; // Initialize it to 0
+
+    if (cartData.data.length === 0) {
+        noDataPresent = 1; // Set it to 1 if cartData.data is empty
+    }
+
     return (
         <>
             <Header />
             <div className='cart-container'>
                 <h2>Cart</h2>
-                <div className="cart-item-table">
-                    <table border="1" className='cart-table'>
-                        <thead>
-                            <tr>
-                                <th>ITEM</th>
-                                <th>PRICE</th>
-                                <th>QUANTITY</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {cartData.data.map((item,index)=>(
-                                <tr key={index}>
-                                    <td>{item.price}</td>
-                                    <td>{item.title}</td>
-                                    <td>{item.quantity} <button onClick={()=>{cartData.removeFromCart(index)}}>Remove</button></td>
+                {noDataPresent !== 1 ? (
+                    <div className="cart-item-table">
+                        <table border="1" className='cart-table'>
+                            <thead>
+                                <tr>
+                                    <th>ITEM</th>
+                                    <th>PRICE</th>
+                                    <th>QUANTITY</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                            </thead>
+                            <tbody>
+                                {cartData.data.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.title}</td>
+                                        <td>{item.price}</td>
+                                        <td>{item.quantity} <button onClick={() => cartData.removeFromCart(item.id)}>Remove</button></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                ) : (
+                    <>No Data Present</>
+                )}
             </div>
             <Footer />
         </>
     )
 }
 
-export default Cart
+export default Cart;
+
--- src/components/contexts/CartItemContext.jsx
+++ src/components/contexts/CartItemContext.jsx
@@ -3,12 +3,15 @@ import React, { createContext, useState } from "react";
 export const CartItemContext = createContext(null);
 
 const CartItemProvider = (props) => {
-  const [count,setCount] = useState(0);
+  const [count, setCount] = useState(0);
   const [data, setData] = useState([]);
 
+
+
+  //function to add product into the cart
   const addToCart = (product) => {
 
-    setCount(count+1);
+    setCount(count + 1);
     const existingData = data.find((item) => item.id === product.id);
     if (existingData) {
       const updatedData = data.map((item) =>
@@ -20,11 +23,27 @@ const CartItemProvider = (props) => {
     }
   };
 
+  //function to remove product from cart
   const removeFromCart = (id) => {
-    const updatedData = data.filter((item) => item.id !== id);
-    setData(updatedData);
+    const productToRemove = data.find((item) => item.id === id);
+  
+    if (productToRemove) {
+      if (productToRemove.quantity > 1) {
+        // If there is more than 1 quantity, decrease the quantity by 1
+        const updatedData = data.map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+        );
+        setData(updatedData);
+      } else {
+        // If there is only 1 quantity, remove the product from the cart
+        const updatedData = data.filter((item) => item.id !== id);
+        setData(updatedData);
+      }
+    }
   };
 
+
+  //object of context 
   const contextValue = {
     addToCart,
     removeFromCart,
@@ -33,6 +52,7 @@ const CartItemProvider = (props) => {
   };
 
   console.log(data);
+
 
   return (
     <CartItemContext.Provider value={contextValue}>
